766f5eb2c06bb8be17da0b61513cace33bc2f428
tallyvanv <talisa.vdv@gmail.com>
2020-07-07 17:18:41 +0200

    added names to form

--- resources/views/new-building/new-building.blade.php
+++ resources/views/new-building/new-building.blade.php
@@ -7,25 +7,25 @@
             <h2>Address</h2>
             <div class="form-group">
                 <label for="inputAddress">Address:</label>
-                <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
+                <input type="text" class="form-control" id="inputAddress" name="inputAddress" placeholder="1234 Main St">
             </div>
             <div class="form-group">
                 <label for="inputAddress2">Address line 2:</label>
-                <input type="text" class="form-control" id="inputAddress2"
+                <input type="text" class="form-control" id="inputAddress2" name="inputAddress2"
                        placeholder="Apartment, studio, or floor">
             </div>
             <div class="form-row">
                 <div class="form-group col-md-6">
                     <label for="inputCity">City:</label>
-                    <input type="text" class="form-control" id="inputCity">
+                    <input type="text" class="form-control" id="inputCity" name="inputCity">
                 </div>
                 <div class="form-group col-md-2">
-                    <label for="inputZip">Post code:</label>
-                    <input type="text" class="form-control" id="inputZip">
+                    <label for="inputPostCode">Post code:</label>
+                    <input type="text" class="form-control" id="inputPostCode" id="inputPostCode">
                 </div>
             </div>
-            <label for="buildingstatus">Building status:</label><br>
-            <input type="text" id="buildingstatus" name="buildingstatus"><br>
+            <label for="buildingStatus">Building status:</label><br>
+            <input type="text" id="buildingStatus" name="buildingStatus"><br>
             <br>
             <button type="submit"  class="btn-success" name="submitNewBuilding">Submit </button>
         </form>
