e42ec3b1bd139913c7b1aa371cfc1229496b9b54
tallyvanv <talisa.vdv@gmail.com>
2020-07-07 10:59:38 +0200

    make dat form

--- resources/views/new-building/new-building.blade.php
+++ resources/views/new-building/new-building.blade.php
@@ -1,45 +1,40 @@
 @extends('layouts.app')
 
 @section('content')
-    <form action="/action_page.php" target="_blank" method="get">
-        <label for="fname">First name:</label><br>
-        <input type="text" id="fname" name="fname" value="John"><br>
-        <label for="lname">Last name:</label><br>
-        <input type="text" id="lname" name="lname" value="Doe"><br><br>
-        <h2>Address</h2>
-        <div class="form-group">
-            <label for="inputAddress">Address</label>
-            <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
-        </div>
-        <div class="form-group">
-            <label for="inputAddress2">Address 2</label>
-            <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
-        </div>
-        <div class="form-row">
-            <div class="form-group col-md-6">
-                <label for="inputCity">City</label>
-                <input type="text" class="form-control" id="inputCity">
+    <div class="container">
+        <form action="{{ route('home') }}" target="_blank" method="post">
+            <label for="buildingname">Name:</label><br>
+            <input type="text" id="buildingname" name="buildingname"><br>
+            <h2>Address</h2>
+            <div class="form-group">
+                <label for="inputAddress">Address:</label>
+                <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
             </div>
-            <div class="form-group col-md-4">
-                <label for="inputState">State</label>
-                <select id="inputState" class="form-control">
-                    <option selected>Choose...</option>
-                    <option>...</option>
-                </select>
+            <div class="form-group">
+                <label for="inputAddress2">Address line 2:</label>
+                <input type="text" class="form-control" id="inputAddress2"
+                       placeholder="Apartment, studio, or floor">
             </div>
-            <div class="form-group col-md-2">
-                <label for="inputZip">Zip</label>
-                <input type="text" class="form-control" id="inputZip">
+            <div class="form-row">
+                <div class="form-group col-md-6">
+                    <label for="inputCity">City:</label>
+                    <input type="text" class="form-control" id="inputCity">
+                </div>
+                <div class="form-group col-md-4">
+                    <label for="inputProvince">Province:</label>
+                    <select id="inputProvince" class="form-control">
+                        <option selected>Choose...</option>
+                        <option></option>
+                    </select>
+                </div>
+                <div class="form-group col-md-2">
+                    <label for="inputZip">Post code:</label>
+                    <input type="text" class="form-control" id="inputZip">
+                </div>
             </div>
-        </div>
-        <div class="form-group">
-            <div class="form-check">
-                <input class="form-check-input" type="checkbox" id="gridCheck">
-                <label class="form-check-label" for="gridCheck">
-                    Check me out
-                </label>
-            </div>
-        </div>
-        <input type="submit" value="Submit">
-    </form>
+            <label for="buildingstatus">Building status:</label><br>
+            <input type="text" id="buildingstatus" name="buildingstatus"><br>
+            <input type="submit" value="Submit" class="btn">
+        </form>
+    </div>
 @endsection
