ee35898988baa1ff134f8d8d8898da89fda1bb7f
tallyvanv <talisa.vdv@gmail.com>
2020-07-08 17:08:56 +0200

    dashboard available

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,12 +5,12 @@
 @section('content')
     <div class="container">
         <div class="row">
-            <div class="col-6 px-2 card" id="userInfo">
-                <div class="row no-gutters d-flex">
-                    <div class="col-auto d-flex flex-center">
-                        <img src="{{ asset('images/coolbuilding.jpg') }}" class="img-circle" alt="">
+            <div class="col-6 card" id="userInfo">
+                <div class="row d-flex">
+                    <div class="col-8 d-flex flex-center align-items-center">
+                        <img src="{{ asset('images/coolbuilding.jpg') }}" alt="">
                     </div>
-                    <div class="col pl-4 d-flex flex-center">
+                    <div class="col-4 d-flex flex-center">
                         <div>
                             <h4>Hi {{ Auth::user()->name }}</h4>
                         </div>
