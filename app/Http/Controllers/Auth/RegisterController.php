57bf8af71657cd80710a8a0714a42a3d7f27fbb1
tallyvanv <talisa.vdv@gmail.com>
2020-07-09 11:09:56 +0200

    dtabase works

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -68,8 +68,8 @@ class RegisterController extends Controller
     protected function create(array $data)
     {
         return User::create([
-            'first_name' => $data['name'],
-            'last_name' => $data['name'],
+            'first_name' => $data['first_name'],
+            'last_name' => $data['last_name'],
             'email' => $data['email'],
             'type' => $data['type'],
             'password' => Hash::make($data['password']),
