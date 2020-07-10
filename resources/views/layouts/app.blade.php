e6fe887cc2a07eee00121afaf171e215a4f69003
Steve Keersmaekers <stevekeersma@gmail.com>
2020-07-10 12:28:08 +0200

    fixing footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,96 +22,109 @@
     <link href="{{ asset('css/footer.css') }}" rel="stylesheet">
 
 </head>
+
 <body>
-<div id="app">
-    <nav class="navbar navbar-expand-md navbar-light shadow-sm" style="background-color: #F6F9FF">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="{{ url('/') }}">
-                <img src="{{url('/images/retracelogo.png')}}" alt="Image"/>
+<div id="page-container">
+    <div id="content-wrap">
+        <div id="app">
+            <nav class="navbar navbar-expand-md navbar-light shadow-sm" style="background-color: #F6F9FF">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="{{ url('/') }}">
+                        <img src="{{url('/images/retracelogo.png')}}" alt="Image"/>
 
-                Re-trace.io
-            </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-                    aria-controls="navbarSupportedContent" aria-expanded="false"
-                    aria-label="{{ __('Toggle navigation') }}">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+                        Re-trace.io
+                    </a>
+                    <button class="navbar-toggler" type="button" data-toggle="collapse"
+                            data-target="#navbarSupportedContent"
+                            aria-controls="navbarSupportedContent" aria-expanded="false"
+                            aria-label="{{ __('Toggle navigation') }}">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
 
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <!-- Left Side Of Navbar -->
-                <ul class="navbar-nav mr-auto">
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <!-- Left Side Of Navbar -->
+                        <ul class="navbar-nav mr-auto">
 
-                </ul>
+                        </ul>
 
-                <!-- Right Side Of Navbar -->
-                <ul class="navbar-nav ml-auto">
-                    <!-- Authentication Links -->
-                    @guest
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                        </li>
-                        @if (Route::has('register'))
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                            </li>
-                        @endif
-                    @else
-                        <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                               data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ Auth::user()->first_name }} <span class="caret"></span>
-                            </a>
+                        <!-- Right Side Of Navbar -->
+                        <ul class="navbar-nav ml-auto">
+                            <!-- Authentication Links -->
+                            @guest
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                </li>
+                                @if (Route::has('register'))
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                    </li>
+                                @endif
+                            @else
+                                <li class="nav-item dropdown">
+                                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                                       data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                        {{ Auth::user()->first_name }} <span class="caret"></span>
+                                    </a>
 
-                            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ route('logout') }}"
-                                   onclick="event.preventDefault();
+                                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                        <a class="dropdown-item" href="{{ route('logout') }}"
+                                           onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                    {{ __('Logout') }}
-                                </a>
-                                <a class="dropdown-item" href="{{ route('home') }}">
-                                    Homepage
-                                </a>
+                                            {{ __('Logout') }}
+                                        </a>
+                                        <a class="dropdown-item" href="{{ route('home') }}">
+                                            Homepage
+                                        </a>
 
 
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST"
-                                      style="display: none;">
-                                    @csrf
-                                </form>
-                            </div>
-                        </li>
-                    @endguest
-                </ul>
-            </div>
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                              style="display: none;">
+                                            @csrf
+                                        </form>
+                                    </div>
+                                </li>
+                            @endguest
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+
+            <main class="py-4">
+                @yield('content')
+            </main>
         </div>
-    </nav>
+        <footer class="site-footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-12 col-md-6">
+                        <h6>About</h6>
+                        <p class="text-justify">Re-trace.io Building Platform</p>
+                    </div>
+                </div>
+                <hr>
+            </div>
+            <div class="container">
+                <div class="row">
 
-    <main class="py-4">
-        @yield('content')
-    </main>
+
+                    <div>
+                        <ul class="social-icons">
+                            <li><a class="socialmedia" href="https://www.instagram.com/re_trace.io/" target="_blank"><i
+                                        class="fa fa-socialmedia"><img src="{{url('/images/instagram.png')}}"
+                                                                       width="25px"
+                                                                       height="auto"></i></a></li>
+                            <li><a class="linkedin" href="https://www.linkedin.com/company/re-trace-io/"
+                                   target="_blank"><i
+                                        class="fa fa-linkedin"><img src="{{url('/images/linkedIn.png')}}" width="25px"
+                                                                    height="auto"></i></a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+    </div>
+    </footer>
 </div>
 </body>
 <!-- Site footer -->
-<footer class="site-footer">
-    <div class="container">
-        <div class="row">
-            <div class="col-sm-12 col-md-6">
-                <h6>About</h6>
-                <p class="text-justify">Re-trace.io Building Platform</p>
-            </div>
-        </div>
-        <hr>
-    </div>
-    <div class="container">
-        <div class="row">
 
-
-            <div>
-                <ul class="social-icons">
-                    <li><a class="socialmedia" href="https://www.instagram.com/re_trace.io/" target="_blank"><i class="fa fa-socialmedia"><img src="{{url('/images/instagram.png')}}" width="25px" height="auto"></i></a></li>
-                    <li><a class="linkedin" href="https://www.linkedin.com/company/re-trace-io/" target="_blank"><i class="fa fa-linkedin"><img src="{{url('/images/linkedIn.png')}}" width="25px" height="auto"></i></a></li>
-                </ul>
-            </div>
-        </div>
-    </div>
-</footer>
 </html>
