ac0af9274caa157cbe0f7d1df9afc74d6f7dd588
tallyvanv <talisa.vdv@gmail.com>
2020-07-13 15:49:02 +0200

    mailer

--- config/mail.php
+++ config/mail.php
@@ -36,11 +36,11 @@ return [
     'mailers' => [
         'smtp' => [
             'transport' => 'smtp',
-            'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
-            'port' => env('MAIL_PORT', 587),
+            'host' => env('MAIL_HOST', 'smtp.mailtrap.io'),
+            'port' => env('MAIL_PORT', 2525),
             'encryption' => env('MAIL_ENCRYPTION', 'tls'),
-            'username' => env('steve.keersmaekers@re-trace.io'),
-            'password' => env('R3trac3st3v3'),
+            'username' => env('MAIL_USERNAME'),
+            'password' => env('MAIL_PASSWORD'),
             'timeout' => null,
             'auth_mode' => null,
         ],
