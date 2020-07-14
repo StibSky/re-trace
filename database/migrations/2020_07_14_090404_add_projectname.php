6f912c9364f48686a4b9427c4c691019db78581c
Steve Keersmaekers <stevekeersma@gmail.com>
2020-07-14 11:05:51 +0200

    updating database

---
+++ database/migrations/2020_07_14_090404_add_projectname.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddProjectname extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('building', function (Blueprint $table) {
+            $table->string("projectName");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('building', function (Blueprint $table) {
+            //
+        });
+    }
+}
