715bb89349cdec36ea256cfad138007d08604132
tallyvanv <talisa.vdv@gmail.com>
2020-07-16 12:36:26 +0200

    new

--- app/Http/Controllers/UpdateAdminController.php
+++ app/Http/Controllers/UpdateAdminController.php
@@ -36,11 +36,19 @@ class UpdateAdminController extends Controller
             $substance->setNameFr($request->input('name_fr'));
             $substance->setSpecificWeight($request->input('specific_weight'));
             $substance->setCode($request->input('code'));
-            $substance->setComments($request->input('comments'));
+            $substance->setComments($request->input('comment'));
             $substance->setParent($request->input('parent'));
             $substance->setIsHazardous($request->input('is_hazardous'));
             $substance->setUnitId($request->input('unit_id'));
-            $substance->save();
+            if (strlen($substance->getCode()) == 4 or strlen($substance->getCode()) == 6 or strlen($substance->getCode()) == 7) {
+                $substance->save();
+                return redirect()->back()->with('success', 'IT WORKS!');
+            }
+            else {
+                return redirect()->back()->with('error', 'Code must be 4, 6 or 7 characters long');
+            }
+
+
 
             //$parent = Substance::where();
             //$substance->setParent($parent->code);
--- resources/views/adminDatabase/admindb.blade.php
+++ resources/views/adminDatabase/admindb.blade.php
@@ -4,6 +4,16 @@
 @endsection
 @section('content')
     <div class="container">
+        @if(session()->has('success'))
+            <div class="alert alert-success">
+                {{ session()->get('success') }}
+            </div>
+        @endif
+            @if(session()->has('error'))
+                <div class="alert alert-danger">
+                    {{ session()->get('error') }}
+                </div>
+            @endif
         <form action="{{ route('saveAdmin') }}" method="post">
             @csrf
             <h2>Basic Info</h2>
