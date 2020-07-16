34d2de42c5c7ff27337c93116ba19c0da9bd65f5
tallyvanv <talisa.vdv@gmail.com>
2020-07-16 09:45:37 +0200

    making controller

--- app/Http/Controllers/UpdateAdminController.php
+++ app/Http/Controllers/UpdateAdminController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Building;
+use App\Substance
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
@@ -19,5 +20,9 @@ class UpdateAdminController extends Controller
 
     public function update(){
 
+    }
+
+    public function addSubstance(){
+        $substance = new Substance();
     }
 }
