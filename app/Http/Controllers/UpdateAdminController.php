09ee396007c27066a3a50fb797a240efacb499fe
Steve Keersmaekers <stevekeersma@gmail.com>
2020-07-16 10:21:17 +0200

    searching for categories by code length

--- app/Http/Controllers/UpdateAdminController.php
+++ app/Http/Controllers/UpdateAdminController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Building;
+use App\Substance;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
@@ -11,11 +11,20 @@ class UpdateAdminController extends Controller
 {
     public function index()
     {
-        
-        return view('adminDatabase.admindb');
+        $substance = Substance::all();
+        $headCategory = Substance::where(DB::raw('LENGTH(code)'), '=', '4')->get();
+        $subCategory1 = Substance::where(DB::raw('LENGTH(code)'), '=', '6')->get();
+        $subCategory2 = Substance::where(DB::raw('LENGTH(code)'), '=', '7')->get();
+
+        return view('adminDatabase.admindb', [
+            'headCategories' => $headCategory,
+            'subCategories1' => $subCategory1,
+            'subCategories2' => $subCategory2
+        ]);
     }
 
-    public function update(){
+    public function update()
+    {
 
     }
 }
