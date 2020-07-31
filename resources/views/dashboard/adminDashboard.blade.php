ff27756abb1767c42b9edf48299127f929543c61
Steve Keersmaekers <stevekeersma@gmail.com>
2020-07-31 11:10:25 +0200

    updating admin dashboard

--- resources/views/dashboard/adminDashboard.blade.php
+++ resources/views/dashboard/adminDashboard.blade.php
@@ -3,42 +3,53 @@
     <link rel="stylesheet" href="{{ asset('css/dashboard_old.css') }}">
 @endsection
 @section('content')
-    <h3>private users</h3>
+    <h2 style="color: red">private users</h2>
 
     @foreach($privates as $private)
-        <h5>Username: "{{$private->first_name}}"</h5>
-        <i>projects</i>
+        <h5 style="color: blue">Username: "{{$private->first_name}}"</h5>
         @if(count($privateBuildings[0]) > 0)
             <ul>
                 @foreach($privateBuildings as $privateBuilding)
-                   @for($i = 0; $i < count($privateBuilding); $i++)
+                    @for($i = 0; $i < count($privateBuilding); $i++)
                         @if($private->id ==$privateBuilding[$i]['userid'] )
 
-                        <li>projectName: " <a
-                                href="{{route('dash', $privateBuilding[$i]->id)}}">{{ $privateBuilding[$i]['projectName'] }}</a>
-                            " <br> type:
-                            "{{$privateBuilding[$i]['type']}}"
-                        </li>
+                            <li> <strong>projectName: </strong> " <a
+                                    href="{{route('dash', $privateBuilding[$i]->id)}}">{{ $privateBuilding[$i]['projectName'] }}</a>
+                                "<br>
+
+                                @if(DB::table('uploaded_file')->where('projectId', $privateBuilding[$i]->id)->first() != null)
+                                This user has files: "<a
+                                    href="{{route('viewFiles', $privateBuilding[$i]->id)}}">Files</a>"
+                                    <br>
+                                @endif
+                                 type:
+                                "{{$privateBuilding[$i]['type']}}"
+                            </li>
                         @endif
                     @endfor
                 @endforeach
             </ul>
         @endif
     @endforeach
-    <h3>business users</h3>
+    <h2 style="color: red">business users</h2>
     @foreach($businesses as $business)
-        <h5>Username: "{{$business->first_name}}"</h5>
-        <i>projects</i>
+        <h5 style="color: blue">Username: "{{$business->first_name}}"</h5>
         @if(count($businessBuildings[0]) > 0)
             <ul>
                 @foreach($businessBuildings as $businessBuilding)
                     @for($i = 0; $i < count($businessBuilding); $i++)
                         @if($business->id ==$businessBuilding[$i]['userid'] )
-                        <li>projectName: "<a
-                                href="{{route('dash', $businessBuilding[$i]->id)}}">{{ $businessBuilding[$i]['projectName'] }} </a>"
-                            <br> type:
-                            "{{$businessBuilding[$i]['type']}}"
-                        </li>
+                            <li><strong>projectName: </strong> "<a
+                                    href="{{route('dash', $businessBuilding[$i]->id)}}">{{ $businessBuilding[$i]['projectName'] }} </a>"
+
+                                @if(DB::table('uploaded_file')->where('projectId', $businessBuilding[$i]->id)->first() != null)
+                                This user has files: "<a
+                                    href="{{route('viewFiles', $businessBuilding[$i]->id)}}">Files page </a>"
+                                @endif
+                                <br>
+                                 type:
+                                "{{$businessBuilding[$i]['type']}}"
+                            </li>
                         @endif
                     @endfor
                 @endforeach
