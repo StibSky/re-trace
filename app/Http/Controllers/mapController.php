21ccff8e9682969e6b01fdc27ba1ba4d0db35ec7
Steve Keersmaekers <stevekeersma@gmail.com>
2020-08-04 11:43:39 +0200

    more maps shit

--- app/Http/Controllers/mapController.php
+++ app/Http/Controllers/mapController.php
@@ -8,9 +8,22 @@ class mapController extends Controller
 {
     function maptest() {
         $response = \GoogleMaps::load('geocoding')
-            ->setParam (['address' =>'santa cruz'])
+            ->setParam (['address' =>'Rode Kruisstraat 11 2220',
+                ])
             ->get();
 
+        $decoded = json_decode($response, true);
+
+        $longName = $decoded['results'][0]['address_components'][0]['long_name'];
+        $shortName = $decoded['results'][0]['address_components'][0]['short_name'];
+
+
+
+
+
+
+
+        var_dump($decoded);
 
         return view('dashboard.map', [
             'response' =>$response
--- resources/views/dashboard/map.blade.php
+++ resources/views/dashboard/map.blade.php
@@ -5,6 +5,27 @@
           integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
 @endsection
 @section('content')
-    <p>Talis is niet mis</p>
-    <div> {{$response}}</div>
+    <style>
+        #map {
+            width: 100%;
+            height: 400px;
+            background-color: grey;
+        }
+    </style>
+    <div id="map" ></div>
+    <script>
+        // Initialize and add the map
+        function initMap() {
+            // The location of Uluru
+            var uluru = {lat: -25.344, lng: 131.036};
+            // The map, centered at Uluru
+            var map = new google.maps.Map(
+                document.getElementById('map'), {zoom: 4, center: uluru});
+            // The marker, positioned at Uluru
+            var marker = new google.maps.Marker({position: uluru, map: map});
+        }
+    </script>
+    <script defer
+            src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAQxZFeQzEx6mmfOypA8Q4uZOU5zmO6lS0&callback=initMap">
+    </script>
 @endsection
