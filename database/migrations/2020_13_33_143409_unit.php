fb5d3fcc170739364e7562a0a5369a29f2d6f232
tallyvanv <talisa.vdv@gmail.com>
2020-08-13 14:46:46 +0200

    migrations

---
+++ database/migrations/2020_13_33_143409_unit.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Unit extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('unit', function (Blueprint $table) {
+            $table->id()->unsigned();
+            $table->string("name");
+            $table->string("name_nl")->nullable();
+            $table->string("name_fr")->nullable();
+            $table->string("short_name");
+            $table->string("short_name_nl")->nullable();
+            $table->string("short_name_fr")->nullable();
+            $table->dateTime("created_at")->default(date("Y-m-d H:i:s"));
+            $table->dateTime("updated_at")->default(date("Y-m-d H:i:s"));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+    }
+}
