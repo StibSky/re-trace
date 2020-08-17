43ee597282555ef2e29055740bbd19e8403a584c
Steve Keersmaekers <stevekeersma@gmail.com>
2020-08-17 10:55:13 +0200

    creating models

---
+++ app/Valuta.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class valuta extends Model
+{
+    private $id;
+    public $timestamps = false;
+    protected $table = "tags";
+
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSymbol()
+    {
+        return $this->symbol;
+    }
+
+    /**
+     * @param mixed $symbol
+     */
+    public function setSymbol($symbol): void
+    {
+        $this->symbol = $symbol;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCreatedAt()
+    {
+        return $this->created_at;
+    }
+
+    /**
+     * @param mixed $created_at
+     */
+    public function setCreatedAt($created_at): void
+    {
+        $this->created_at = $created_at;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updated_at;
+    }
+
+    /**
+     * @param mixed $updated_at
+     */
+    public function setUpdatedAt($updated_at): void
+    {
+        $this->updated_at = $updated_at;
+    }
+
+    protected $fillable = [
+        'name', 'symbol', 'created_at',
+        'updated_at'
+    ];
+
+}
