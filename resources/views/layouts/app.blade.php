86eda69bff46d4425c03e0e26226f0829dfeeabc
tallyvanv <talisa.vdv@gmail.com>
2020-09-01 17:16:27 +0200

    layout stuff

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -113,7 +113,7 @@ sets up the navbar and yields the content of the other pages
             </nav>
 
             <main class="py-4">
-                <div class="container">
+                <div class="container-fluid px-5">
                     @if(session()->has('success'))
                         <div class="alert alert-success">
                             {{ session()->get('success') }}
