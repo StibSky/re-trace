59ee8bdac9af4171cd799af3e23a6e28665fb4c0
tallyvanv <talisa.vdv@gmail.com>
2020-09-14 10:37:39 +0200

    please javascript work

--- resources/views/profile-page/home.blade.php
+++ resources/views/profile-page/home.blade.php
@@ -19,8 +19,8 @@
 @endsection
 @section('head-script')
     {{--    <script src="https://unpkg.com/masonry-layout@4/dist/masonry.pkgd.min.js"></script>--}}
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.bundle.min.js"></script>
+{{--    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.bundle.min.js"></script>--}}
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-select/1.13.1/js/bootstrap-select.min.js"></script>
 @endsection
 @section('content')
