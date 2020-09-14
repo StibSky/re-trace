b1c11b1ff26dc5e578b138b54a0092cc29f9f04a
Steve Keersmaekers <stevekeersma@gmail.com>
2020-09-14 10:38:33 +0200

    merge

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,7 +6,7 @@ import Dropdown from 'vue-simple-search-dropdown';
 
 Vue.use(Dropdown);
 
-Vue.component('example-component', require('./components/ExampleComponent.vue'));
+Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
 /* Register our new component: */
 Vue.component('stream-form', require('./components/StreamForm.vue').default);
