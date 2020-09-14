c86073d2d969dc9ef849da69eaa8d09bfdc7f453
tallyvanv <talisa.vdv@gmail.com>
2020-09-14 10:32:06 +0200

    pull from master

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,6 +15,8 @@ sets up the navbar and yields the content of the other pages
 
     <!-- Scripts -->
     <script type="text/javascript" src="{{ asset('/js/bootstrap') }}"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.bundle.min.js"></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -159,14 +161,14 @@ sets up the navbar and yields the content of the other pages
         </footer>
     </div>
 </div>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
-<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
+{{--<script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>--}}
+{{--<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js"
         integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n"
         crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"
         integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6"
-        crossorigin="anonymous"></script>
-<script src="{{mix('/js/app.js')}}"></script>
+        crossorigin="anonymous"></script>--}}
+<script src="{{ asset('/js/app.js')}}"></script>
 @stack('script')
 </body>
 <!-- Site footer -->
